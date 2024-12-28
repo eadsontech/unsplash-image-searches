c46fb9b3b7d9aff8283f3bece4b275663d9a08f7
Alexander <eadsontech@gmail.com>
2024-12-28 20:54:16 +0100

    api removed

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -2,7 +2,6 @@ import { useQuery } from "@tanstack/react-query";
 import axios from 'axios';
 import { useGlobalContext } from "./context";
 
-const url = `https://api.unsplash.com/search/photos?client_id=${import.meta.env.VITE_API_KEY}`;
 
 
 
